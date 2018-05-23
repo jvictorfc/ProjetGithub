f964748e518bf51bc43caf107a5eb06a75f27440
1795928 <1795928@LI-S064-0T05>
2018-05-23 09:12:34 -0400

    Student class creation

--- src/refactoringExemple/Lab.java
+++ src/refactoringExemple/Lab.java
@@ -10,5 +10,33 @@ package refactoringExemple;
  * @author 1795928
  */
 public class Lab {
+    String idClass;
+    Integer capacity;
+    Integer nbComputers;
+
+    public String getIdClass() {
+        return idClass;
+    }
+
+    public void setIdClass(String idClass) {
+        this.idClass = idClass;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public Integer getNbComputers() {
+        return nbComputers;
+    }
+
+    public void setNbComputers(Integer nbComputers) {
+        this.nbComputers = nbComputers;
+    }
+    
     
 }
--- src/refactoringExemple/Student.java
+++ src/refactoringExemple/Student.java
@@ -5,10 +5,46 @@
  */
 package refactoringExemple;
 
+import java.util.Date;
+
 /**
  *
  * @author 1795928
  */
-public class Student {
+public class Student extends Person{
+    String student_id;
+    
+    String lastName;
+    String firstName;
+    Date dateBirth;
+    
+    public Student(String n, String p, Date d){
+        this.lastName=n;
+        this.firstName=p;
+        this.dateBirth=d;
+        this.student_id=createStudentId(n,p,d);
+    }
+    
+    public String createStudentId(String lName, String fName, Date date){
+        String end = concatDate(date);
+        return lName.substring(0,3) + fName.charAt(0) + end;
+    }
+    
+    private String concatDate(Date date){
+        return date.getYear() + "-" + date.getMonth();
+    }
+    
+    public String getStudentId(String student_id){
+        return student_id;
+    }
+    
+    public void setStudentId(){
+        this.student_id = student_id;
+    }
+    
+    public void isSupervisedBy(Professor professor){
+        professor.students.add(this);
+    }
+    
     
 }
